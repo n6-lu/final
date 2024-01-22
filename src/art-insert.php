386f52434fc669f1a1f97b48fa5d2ddc9dd68efa
n6-lu <2201130@s.asojuku.ac.jp>
2024-01-22 22:22:56 +0900

    Create art-insert.php

---
+++ src/art-insert.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title></title>
+</head>
+<body>
+    <h2>作品を追加</h2>
+    <a href="art-menu.php">メニューに戻る</a>
+    <hr>
+    <form action="art-insert2.php" method="post">
+            作品名：<input type="text" name="title"><br>
+            作者：<input type="text" name="authorname"><br>
+            <p><button type="submit">登録</button></p>
+        </form>
+    </body>
