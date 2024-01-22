51c0d2a37622501bc2f0f84faf593e3c8e04c2f6
n6-lu <2201130@s.asojuku.ac.jp>
2024-01-22 22:22:51 +0900

    Create art-delete.php

---
+++ src/art-delete.php
@@ -0,0 +1,39 @@
+<?php
+    const SERVER = 'mysql220.phy.lolipop.lan';
+    const DBNAME = 'LAA1517456-final';
+    const USER = 'LAA1517456';
+    const PASS = 'Pass0506';
+
+    $connect = 'mysql:host='. SERVER . ';dbname='. DBNAME . ';charset=utf8';
+?>
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title></title>
+</head>
+<body>
+    <h2>作品一覧</h2>
+    <a href="art-menu.php">メニューに戻る</a>
+    <hr>
+    <?php
+        $pdo=new PDO($connect, USER, PASS);
+        foreach ($pdo->query('select * from art') as $row) {
+            echo '<tr>';
+            echo '<td>', $row['id'], '</td>';
+            echo '<td>', $row['title'], '</td>';
+            echo '<td>', $row['authorname'], '</td>';
+            echo '<td>';
+            echo '<form action="art-delete2.php" method="post">';
+            echo '<input type="hidden" name="id" value="', $row['id'], '">';
+            echo '<button type="submit">削除</button>';
+            echo '</form>';
+            echo '</td>';
+            echo '</tr>';
+            echo "\n";
+        }
+        ?>
+    </body>
+</html>
